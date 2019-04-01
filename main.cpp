136e12e944ac9b26a93e00a8ba56e3bbccff36d3
Rjhans7 <roosevelt.ubaldo@outlook.com>
2019-03-31 23:25:51 -0500

    version estable

--- main.cpp
+++ main.cpp
@@ -1,7 +1,28 @@
 #include <iostream>
+#include <vector>
+#include <sstream>
+
 
 using namespace std;
 
 int main(int argc, char *argv[]) {
+    string input;
+    char letra;
+        bool first = true;
+        while (cin.get(letra)) {
+            if (letra == '"') {
+                if (first) {
+                    cout << "``";
+                    first = false;
+                } else {
+                    cout << "''";
+                    first = true;
+                }
+            } else {
+                cout << letra;
+            }
+        }
+
+
     return 0;
 }
